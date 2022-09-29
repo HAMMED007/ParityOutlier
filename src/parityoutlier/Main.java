2bd5b72f5975f2617fa75238cb78acec10610094
HAMMED007 <104014722+HAMMED007@users.noreply.github.com>
2022-09-29 15:36:54 +0100

    Update Main.java

--- src/parityoutlier/Main.java
+++ src/parityoutlier/Main.java
@@ -55,3 +55,48 @@ public class FindOutlier {
         return Math.max(result1,result2);
     }}
 }
+
+/*
+
+HERE IS ANOTHER FUNCTION WITHOUT ARRAYLIST
+
+public class FindOutlier {
+    static int find(int[] integers) {
+
+        int m = 0, n1 = 0;    //Declare the size of the array for even and odd elements
+        for (int i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 == 0)
+                m++;    //Increment even array size
+            else
+                n1++;   //Increment odd array size
+        }
+        int even[] = new int[m];    //Declare an even array
+        int odd[] = new int[n1];   //Declare an odd array
+
+        int k = 0, t = 0;
+        for (int i = 0; i < integers.length; i++) {
+            if (integers[i] % 2 == 0) {
+                even[k] = integers[i];   //Initialize elements of even array
+                k++;
+            } else {
+                odd[t] = integers[i];  //Initialize elements of odd array
+                t++;
+            }
+        }
+        int result1 = 0;
+        int result2 = 0;
+
+        if (odd.length < even.length){
+            for (int i = 0; i < odd.length; i++) {
+                result1 = odd[0];
+            }
+        }else
+            for (int i = 0; i < even.length; i++) {
+                result1 = even[0];
+
+
+            }
+        return Math.max(result1,result2);
+    }
+}
+*/
