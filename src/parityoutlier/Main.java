b1fdbfb943f3faa3995abbd4ea7b0fca2dfb029a
HAMMED007 <104014722+HAMMED007@users.noreply.github.com>
2022-09-29 15:02:42 +0100

    Update Main.java

--- src/parityoutlier/Main.java
+++ src/parityoutlier/Main.java
@@ -21,36 +21,37 @@ public class Main {
 
 }
 
-    public static class FindOutlier{
-       static int find(int[] integers) {
-            ArrayList<Integer> odd_number = new ArrayList();
-            ArrayList<Integer> even_number = new ArrayList();
-            int even_total = 0;
-            int odd_total = 0;
+public class FindOutlier {
+    static int find(int[] integers) {
+        ArrayList<Integer> odd_number = new ArrayList();
+        ArrayList<Integer> even_number = new ArrayList();
+        int result1 = 0;
+        int result2 = 0;
 
-            for (int i = 0; i < integers.length; i++) {
 
-                if (integers[i]%2 == 0){
-                    even_number.add(integers[i]);
+        for (int i = 0; i < integers.length; i++) {
 
-                }else
-                    odd_number.add(integers[i]);
+            if (integers[i] % 2 == 0) {
+                even_number.add(integers[i]);
 
-            }
-            for (int i = 0; i < even_number.size(); i++) {
-                even_total = even_total + even_number.get(i);
-            }
+            } else
+                odd_number.add(integers[i]);
+
+
+
+
+
+    }
+        if (odd_number.size() < even_number.size()){
             for (int i = 0; i < odd_number.size(); i++) {
-                odd_total = odd_total + odd_number.get(i);
+                result1 = odd_number.get(i);
             }
+        }else
+            for (int i = 0; i < even_number.size(); i++) {
+                result2 = even_number.get(i);
 
 
-            return Math.min(odd_total,even_total);
-        }
-
-
-
-
-
-}
+    }
+        return Math.max(result1,result2);
+    }}
 }
